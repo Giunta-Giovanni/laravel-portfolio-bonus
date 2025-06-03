48f8203edb5dd56ef21a15375ea7adb7f3afce1a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-03 19:31:42 +0200

    Refactor Header component to include responsive navigation and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
+import { NavLink } from "react-router-dom"
+import portfolio from "../assets/svg/portfolio.svg"
 export default function Header() {
     return (
         <header>
+            <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div className="container">
+                    <NavLink className="navbar-brand d-flex align-items-center " to="/">
+                        <img src={portfolio} className="logo" alt="" />
+                    </NavLink>
 
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/projects">Progetti</NavLink>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
     )
 }
